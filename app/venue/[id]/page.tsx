a9c5e8aa86ffb7723f7bba029a54fbbb58941c83
Rowe <ruvier20144@gmail.com>
2025-12-23 22:47:46 +0800

    improved venue clicking with sign in options

--- app/venue/[id]/page.tsx
+++ app/venue/[id]/page.tsx
@@ -1740,6 +1740,16 @@ export default function VenueDetails() {
                     }
                   }}
                 >
+                  <span style={{ display: 'inline-flex', alignItems: 'center', marginRight: 8 }}>
+                    <svg width="20" height="20" viewBox="0 0 48 48" style={{ display: 'block' }}>
+                      <g>
+                        <path fill="#4285F4" d="M43.6 20.5h-1.9V20H24v8h11.3c-1.6 4.3-5.7 7-11.3 7-6.6 0-12-5.4-12-12s5.4-12 12-12c2.7 0 5.2.9 7.2 2.4l6-6C34.5 5.1 29.5 3 24 3 12.4 3 3 12.4 3 24s9.4 21 21 21c10.5 0 20-7.7 20-21 0-1.4-.2-2.7-.4-3.5z"/>
+                        <path fill="#34A853" d="M6.3 14.7l6.6 4.8C14.5 16.1 18.9 13 24 13c2.7 0 5.2.9 7.2 2.4l6-6C34.5 5.1 29.5 3 24 3 16.3 3 9.3 7.6 6.3 14.7z"/>
+                        <path fill="#FBBC05" d="M24 45c5.4 0 10.4-1.8 14.3-4.9l-6.6-5.4C29.5 36.9 26.9 38 24 38c-5.5 0-10.2-3.7-11.8-8.7l-6.5 5C9.3 40.4 16.3 45 24 45z"/>
+                        <path fill="#EA4335" d="M43.6 20.5h-1.9V20H24v8h11.3c-1.1 3-3.5 5.2-6.3 6.5l6.6 5.4C39.7 37.1 45 32.5 45 24c0-1.4-.2-2.7-.4-3.5z"/>
+                      </g>
+                    </svg>
+                  </span>
                   Continue with Google
                 </button>
                 <button
